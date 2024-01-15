9743771eb9cfdeb859beaf5658fd2208d154165b
akadjsam <akadjsam@gmail.com>
2024-01-15 13:21:23 +0900

    v0.7 python day02

--- day02.py
+++ day02.py
@@ -8,5 +8,14 @@
 
 base_number = int(input('Input base number : '))
 exponent_number = int(input('Input exponent number : '))
+# f-string
+# print(f'base is {base_number}, exponent is {exponent_number}, result is {base_number ** exponent_number}') p.79
+print(f'base is {base_number}, exponent is {exponent_number}, result is {pow(base_number, exponent_number)}')
 
-print(f'base is {base_number}, exponent is {exponent_number}, result is {base_number ** exponent_number}')
+#format
+print("base is {0}, exponent is {1}, result is {2}".format(base_number, exponent_number, pow(base_number, exponent_number)))
+print("base is {}, exponent is {}, result is {}".format(base_number, exponent_number, pow(base_number, exponent_number)))
+
+# c like
+print("base is %d, exponent is %d, result is %d" %(base_number, exponent_number, pow(base_number, exponent_number)))
+
