7b0e67b63ecdceaf5e41e36064ce39081dad1486
akadjsam <akadjsam@gmail.com>
2024-01-15 10:44:52 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,15 @@
+a = "inha"
+print(a)
+print(a[3])
+# a[3] = "U" #immutable
+#
+sub = ['python','c++','linux']
+print(sub)
+print(sub[0])
+sub[0] = "database" #mutable p.63
+print(sub)
+
+# print(0.1)
+# print(1e-1)
+# print(3.141592)
+# print(0.3141592e3)
