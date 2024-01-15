a0eddc17b598d89fb56b884b46c734e3bc8fd791
akadjsam <akadjsam@gmail.com>
2024-01-15 15:42:18 +0900

    v1.2 python day02

--- day02.py
+++ day02.py
@@ -1,8 +1,13 @@
 #(32°F − 32) × 5/9 = 0°C
 
 #(0°C × 9/5) + 32 = 32°F
+temp = []
+if temp:
+    print("full") #temp = [0]
+else:
+    print("empty")
 
-menu = input("1) Fahrenheit to Celsius\n2) Celsius to Fahrenheit\n3) Quit program : ")
+menu = input("1) Fahrenheit to Celsius\n2) Celsius to Fahrenheit\n3) Quit program\n")
 
 if menu == '1':
     fahrenheit = float(input("Input Fahrenheit : "))
@@ -11,3 +16,5 @@ if menu == '1':
 elif menu =='2':
     Celsius = float(input("Input Fahrenheit : "))
     print(f'Celsius : {Celsius}C, Fahrenheit : {((Celsius * 9.0/5.0) + 32.0):.4f}F')
+else:
+    print("Terminate program")
