2e69277db73096d94ba03a15dd81afc4ce0cd711
akadjsam <akadjsam@gmail.com>
2024-01-16 14:03:10 +0900

    v2.2 prime number day03

--- day03.py
+++ day03.py
@@ -1,10 +1,16 @@
-#예외처리 preview
-sub = "python c++ database linux"
-print("%e"%0.703)
-print(sub.isalnum()) # 공백, +가 있기때문에 false, 오직 알파벳 혹은 숫자로 이루어져 있어야 한다.
-sub_input = input("수강신청과목 입력 : ")
-try:
-    print(f'해당 과목은 존재합니다. 위치는 {sub.index(sub_input)}번 인덱스입니다.') #sub.find(sub_input)로 대체 가능.
-except ValueError:
-    print("해당과목이 존재하지 않습니다.")
+# sub = {"python" : "kim", "c++" : "sung", "datastructure" : "kim","database" : "kang"}
+# sub2 = {"python" : "kim", "c++" : "sung", "datastructure" : "kim","database" : "kang"}
+# print("{0[python]}, {1[datastructure]}".format(sub,sub2))
 
+#prime number(솟수)
+number = int(input("Input number : "))
+count = 0
+i = 1
+while i<=number:
+    if number%i == 0:
+        count += 1
+    i += 1
+if count == 2:
+    print(f"{number} is prime number")
+else:
+    print(f"{number} is not prime number")
