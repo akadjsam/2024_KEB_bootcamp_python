845ad880bd73ef1fee53ff56080e4fab7ffcda9e
akadjsam <akadjsam@gmail.com>
2024-01-16 15:27:12 +0900

    v2.9 prime number day03

--- day03.py
+++ day03.py
@@ -1,16 +1,15 @@
 #prime number(솟수)
-number = int(input("Input number : "))
-isPrime = True
-if number < 2:
-    print(f"{number} is not prime number")
-else:
-    for i in range(2,number):
-        if number%i == 0:
-            isPrime = False
-            break
-
-
-if isPrime: #remove count == 0
-    print(f"{number} is prime number")
-else:
-    print(f"{number} is not prime number")
+numbers = input("Input First number and Second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+for number in range(n1,n2+1):
+    isPrime = True
+    if number < 2:
+        pass
+    else:
+        for i in range(2,number):
+            if number%i == 0:
+                isPrime = False
+                break
+        if isPrime:
+            print(number, end=' ')
