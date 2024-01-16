a5f89d234c9b935ca7d1c7b23bec0c190b1fd847
akadjsam <akadjsam@gmail.com>
2024-01-16 11:44:47 +0900

    v2.0 day03

--- day03.py
+++ day03.py
@@ -1,6 +1,8 @@
+#예외처리 preview
 sub = "python c++ database linux"
 sub_input = input("수강신청과목 입력 : ")
-if sub.find(sub_input) != -1:
+try:
     print(f'해당 과목은 존재합니다. 위치는 {sub.index(sub_input)}번 인덱스입니다.') #sub.find(sub_input)로 대체 가능.
-else:
+except ValueError:
     print("해당과목이 존재하지 않습니다.")
+
