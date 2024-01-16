09fd53be75c1211cb4c165ade5958a405a2aa86b
akadjsam <akadjsam@gmail.com>
2024-01-16 10:59:27 +0900

    v1.7 day03

--- day03.py
+++ day03.py
@@ -1,13 +1,17 @@
-university = "Inha\nUniversity!"
-# university = r"Inha\nUniversity!" #raw string
-#print(university)
-print(university[:4])
-print(university[:-11])
-print(university[:len(university)])
-print(university[::2])
+# course = "2024 KEB Bootcamp"
+# print(course)
+# list_course = course.split() #아무것도 입력하지 않으면 스페이스 기준으로 리스트에 담기
+# list_course = course.split('B') # 출력 결과 : ['2024 KE', ' ', 'ootcamp'] B를 구분기호로 지정
+# print(list_course)
 
-number1 = input("first number")
-number2 = input("first number")
-print(number1+number2) #concataion
-print(number1*3)
-#print(number1+3) #error type missmatch
+# split : string to list
+# join : list to string
+
+# num = input("First number Second number").split()
+# print(type(num)) #num's type is 'list'
+# # print(num[0]+num[1]) #concatenation
+# print(int(num[0])+int(num[1])) #arithmetic opeartion
+
+sub = ["python", "c++", "database"]
+sub = "/ ".join(sub)
+print(sub)
