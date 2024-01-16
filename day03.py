867a5df72a56fb61680c15764d177dd533c434df
akadjsam <akadjsam@gmail.com>
2024-01-16 11:39:38 +0900

    v1.9 day03

--- day03.py
+++ day03.py
@@ -1,21 +1,6 @@
-# course = "2024 KEB Bootcamp"
-# print(course.replace('KEB', 'INHA'))
-# print(course) #output : 2024 KEB Bootcamp (immutable)
-# course = course.replace('KEB', 'INHA')
-# print(course)
-
-# course = "KEB 2024 KEB Bootcamp"
-# course = course.replace('KEB', 'INHA',2) #3번째 인수는 변경할 숫자를 의미함.
-# print(course)
-
-# course = "*KEB 2024# KEB !Bootcamp...*!#"
-# course = course.strip("!#.*") #연속적으로 되어 있는 것들만 삭제(끝에 있는 것들)
-# print(course)
-
-course = "* KEB 2024# KEB !Bootcamp KEB...*!#"
-print(course.find('KEB'))
-print(course.rfind('KEB')) #reverse
-print(course.index('KEB'))
-print(course.rindex('KEB')) #reverse
-print(course.find('inha')) # -1 return
-# print(course.index('inha')) # value error
+sub = "python c++ database linux"
+sub_input = input("수강신청과목 입력 : ")
+if sub.find(sub_input) != -1:
+    print(f'해당 과목은 존재합니다. 위치는 {sub.index(sub_input)}번 인덱스입니다.') #sub.find(sub_input)로 대체 가능.
+else:
+    print("해당과목이 존재하지 않습니다.")
