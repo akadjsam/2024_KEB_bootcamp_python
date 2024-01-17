d1a66d5bd96192aa09ba1d6dab44adb83f0e6971
akadjsam <akadjsam@gmail.com>
2024-01-17 15:34:01 +0900

    v4.1 day04

--- day04.py
+++ day04.py
@@ -1,22 +1,26 @@
-sugang = dict(python="kim",cpp="sung",db="kang")
-# print(sugang)
-# sugang['datastructure'] = 'kim'
-# print(sugang)
-# sugang['datastructure'] = 'park'
-# print(sugang)
-#
-# print(sugang['db'])
-# print(sugang.get("db")) # get으로 해당 키에 해당하는 벨류를 출력
-# print(sugang.get("opensource")) #찾는 키값이 없으먼 none 리턴
-# print(sugang.get("opensource", "not exist")) #찾는 키 값이 없으면 , 뒤의 값을 출력
+import random
 
-for sub, professor in sugang.items(): #모든 키/벨류값을 가져온다.
-    print(f'{sub} : {professor}') #딕셔너리에는 순서의 개념이 없다.
+drinks_foods = {"위스키" : "초콜릿", "와인" : "치즈", "소주" : "삼겹살", "고량주" : "양꼬치"}
+drinks_foods_keys = list(drinks_foods.keys())
+#drink = input(drinks_foods.keys())
+#print(drinks_foods[drink])
 
-for i in sugang.keys(): #모든 키 가져오기 for i in sugang: 과 같게 동작한다.
-    print(i)
-for v in sugang.values(): #모든 벨류 가져오기
-    print(v)
-
-for s in sugang.items(): #하나로 받게 되면 튜플형태로 받는다.
-    print(s) #튜플로 출력
+while True:
+    menu = input(f'다음 술 중에 고르세요.\n1){drinks_foods_keys[0]}, 2){drinks_foods_keys[1]},'
+                 f' 3){drinks_foods_keys[2]}, 4){drinks_foods_keys[3]}, 5)아무거나 6)종료 : ')
+    #random.choice(drinks_foods_keys)
+    #menu = input(f'다음 술 중에 고르세요.\n{", ".join(drinks_foods_keys)}')
+    if menu == '1':
+        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다.')
+    elif menu == '2':
+        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다.')
+    elif menu == '3':
+        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다.')
+    elif menu == '4':
+        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다.')
+    elif menu == '5':
+        random_drink = random.choice(drinks_foods_keys)
+        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다.')
+    elif menu == '6':
+        print("종료")
+        break
