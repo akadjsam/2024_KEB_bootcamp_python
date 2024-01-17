2af15f1f349cb72817765a718d05016be891f65e
akadjsam <akadjsam@gmail.com>
2024-01-17 14:31:16 +0900

    v3.9 day04

--- day04.py
+++ day04.py
@@ -1,28 +1,13 @@
-# #sub = ['a','b','c']
-# sub = ['a',['b','c'],'d']
-# a = sub
-# b = sub.copy()
-# c = list(sub)
-# d = sub[:]
-#
-# print(sub,a,b,c,d)
-# sub[1][1] = "x"
-# print(sub,a,b,c,d) #얕은복사
-#
+#normal
+# squares1 = []
+# for i in range(1,7):
+#     squares1.append(i+1)
+# print(squares1)
 
-import copy
-# #sub = ['a','b','c']
-sub = ['a',['b','c'],'d']
-a = sub
-b = sub.copy()
-c = list(sub)
-d = sub[:] #a~d 얕은복사
-e = copy.deepcopy(a) #깊은복사
-print(sub,a,b,c,d)
-sub[1][1] = "x"
-print(sub,a,b,c,d,e)
+#list comprehension
+squares2 = [i*i for i in range(1,7) if i % 2 == 1] #리스트 컴프레션에 홀수만 출력 조건 추가
+print(squares2)
 
-eng = "a","b","c"
-fre = 'd','f','g'
-z = dict(zip(eng,fre))
-print(z)
+# a = {[2]: [1,2], "b" : (1,2)} #딕셔너리의 키 값은 불변이어야 한다. (set, list, dict 등등 불가능)
+# print(type(a.get([2])))
+#x = dict(fi rst="123",def="456") #딕셔너리의 키 값은 공백이나 예약어가 올 수 없다.
