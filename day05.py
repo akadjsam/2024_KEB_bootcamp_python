6dffce433906144e2a7d4c0c3b9a6d89f90c9e5d
akadjsam <akadjsam@gmail.com>
2024-01-18 09:25:18 +0900

    v4.3 assignment day05

---
+++ day05.py
@@ -0,0 +1,13 @@
+# univ= 'inha university'
+# counts_alphabet = {letter : univ.count(letter) for letter in univ}
+# print(counts_alphabet)
+
+# univ= 'inha university'
+# counts_alphabet = dict()
+# for i in univ:
+#     counts_alphabet[i] = univ.count(i)
+# print(counts_alphabet)
+
+#assignment
+squares = {n : n**2 for n in range(10)} #n*n = pow(n,2) = n**2
+print(squares)
