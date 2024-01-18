a530ce641a99bbd514e16f330434e0f970604475
akadjsam <akadjsam@gmail.com>
2024-01-18 10:52:27 +0900

    v4.4 prime number function day05

--- day05.py
+++ day05.py
@@ -1,13 +1,28 @@
-# univ= 'inha university'
-# counts_alphabet = {letter : univ.count(letter) for letter in univ}
-# print(counts_alphabet)
+#prime number(솟수)
 
-# univ= 'inha university'
-# counts_alphabet = dict()
-# for i in univ:
-#     counts_alphabet[i] = univ.count(i)
-# print(counts_alphabet)
+def isPrime(n) -> bool: # -> bool 은 리턴타입이 무엇인지 알려주는 역할, 안써도 상관은 없다.
+    """
+    매개변수로 넘겨 받은 수가 소수 여부를 boolean type으로 리턴한다
+    :param n: 판정할 매개변수
+    :return: 소수이면 true, 아니면 false return. 
+    """ #가이드라인 help(isPrime)
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i**2 <= n:
+            if n%i == 0:
+                return False
+            i += 1
+        return True
 
-#assignment
-squares = {n : n**2 for n in range(10)} #n*n = pow(n,2) = n**2
-print(squares)
+numbers = input("Input First number and Second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
+if n1 > n2:
+    n1, n2 = n2, n1
+for number in range(n1,n2+1):
+    if isPrime(number):
+        print(number, end=' ')
+
+
