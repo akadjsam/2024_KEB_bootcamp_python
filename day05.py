9970ca6e77b05aee607c3588ed76eb16e99f34ed
akadjsam <akadjsam@gmail.com>
2024-01-18 18:51:24 +0900

    v5.0 day05

--- day05.py
+++ day05.py
@@ -1,9 +1,30 @@
+# def my_range(first=0,last=10,step=1):
+#     number = first
+#     while number < last:
+#         yield number
+#         number += step
+#
+# r = my_range()
+# print(r,type(r))
+# for i in r:
+#     print(i)
+#
+# for i in r: #이 반복문은 실행되지 않음
+#     print(i)
+
+def deco(func):
+    def new_func(a):
+        print(func.__doc__)
+        result = func(a)
+        return result
+    return new_func
+@deco
 def squares(n):
+    """
+    제곱근
+    :param n: int
+    :return: int
+    """
     return n*n
 
-even_numbers = [i for i in range(51) if i%2==0]
-
-print(tuple(map(squares,even_numbers)))
-print(tuple(map(lambda x : x**2, even_numbers)))
-z = lambda x : pow(x,2)
-print(tuple(map(z, even_numbers)))
+print(squares(5))
