669ac075bc126b68ac208123178e8fec6d87373e
akadjsam <akadjsam@gmail.com>
2024-01-19 09:30:06 +0900

    v5.4 assignment 9.2 day06

--- day06.py
+++ day06.py
@@ -1,15 +1,19 @@
-def get_odds(n) -> int:
+def test(f):
     """
-    1 부터 n까지의 홀수를 발생시키는 제너레이터
-    :param n: int
-    :return: int
+    decorator 함수 시작하면 start 출력, 함수 끝나면 end 출력
+    :param f: function
+    :return: closure function
     """
-    for i in range(1,n,2):
-        yield i
-cnt = 0
-odds = get_odds(10)
-for i in odds:
-    cnt += 1
-    if cnt == 3:
-        print(i)
-        break
+    #def test_in():
+    def test_in(*args,**kwargs):
+        print('start')
+        #f() 이렇게 하면 리턴 생략 가능
+        result = f(*args,**kwargs)
+        print('end')
+        return result
+    return test_in
+@test
+def greeting():
+    print("안녕하세요")
+
+greeting()
