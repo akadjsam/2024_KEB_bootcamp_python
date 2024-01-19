3d7199b4b71b75d62f909b29de7180a21d3a729b
akadjsam <akadjsam@gmail.com>
2024-01-19 14:43:18 +0900

    v6.1 day06

--- day06.py
+++ day06.py
@@ -1,25 +1,33 @@
 class Pokemon:
-    #self : 실행시점의 객체의 메모리를 가지는 것을 의미한다.
-    def __init__(self, name): #생성자, 객체 생성시 실행되는 함수 엄밀히 따지자면 생성자보단 초기화에 더 가깝다. __new__가 사실상 생성자임
-        #is-a:상속관계
-        #has-a:연관관계
-
+    def __init__(self,name):
         self.name = name
-        print(f"{name} 포켓몬스터 생성")
-    def attack(self, target):
+    def attack(self,target):
         print(f'{self.name}이(가) {target.name}을(를) 공격!')
 
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle = Pokemon("꼬부기")
-charizard.attack(squirtle)
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# #print(pikachu.nemesis.name)
-# pikachu.nemesis.name = "꼬부기" #squirtle.name = "꼬부기"
-# print(pikachu.nemesis.name)
-# print(squirtle.name)
+class Pikachu(Pokemon): #is-a 상속관계
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.type = type
 
-print(pikachu.name)
-print(squirtle.name)
+    def attack(self, target):
+        print(f'{self.name}이(가) {target.name}에게 {self.type} 공격!')
+    # def electric_info(self):
+    #     print("전기계열의 공격을 실행")
+
+class Squirtle(Pokemon): #is-a 상속관계
+    pass #메서드 오버라이드 하지 않고 사용하면 부모의 메서드를 찾아가서 사용한다.
+    #def attack(self, target):
+        #print(f'{self.name}이(가) {target.name}에게 전기 공격!')
+
+class Agumon:
+    pass
+
+p1 = Pikachu("피카츄", "전기")
+s1 = Squirtle("꼬부기")
+#p3 = Pokemon("아무개")
+#p3.electric_info() #자식의 메서드를 사용하지 못한다.
+p1.attack(s1)
+s1.attack(p1)
+print(p1.name,p1.type)
+print(issubclass(Pikachu, Pokemon))
+print(issubclass(Agumon, Pokemon)) #상속관계인지 확인할 때 사용한다. (boolean type)
