562bfbe30f36be13364e50cc3dc9006913afdf85
akadjsam <akadjsam@gmail.com>
2024-01-19 15:25:16 +0900

    v6.5 day06

--- day06.py
+++ day06.py
@@ -1,20 +1,22 @@
 class FlyinMinin:
     def fly(self):
-        return print(f'{self.name}이(가) 날아갑니다.')
+        return print(f'{self.hidden_name}이(가) 날아갑니다.')
 class SwimmingMixin:
     def swim(self):
-        return print(f'{self.name}이(가) 수영을 합니다.')
+        return print(f'{self.hidden_name}이(가) 수영을 합니다.')
 class Pokemon:
     def __init__(self,name):
-        self.name = name
+        self.hidden_name = name
     def attack(self):
         print("공격")
     def get_name(self):
         print("inside getter")
-        return self.name
+        return self.hidden_name
     def set_name(self,new_name):
         print("inside setter")
-        self.name = new_name
+        self.hidden_name = new_name
+
+    name = property(get_name,set_name)
 class Charizard(Pokemon, FlyinMinin):
     pass
 class Gyarados(Pokemon, SwimmingMixin):
@@ -27,6 +29,11 @@ c1 = Charizard("리자몽")
 # c1.attack()
 # #Charizard.attack() #에러. 이렇게 사용하기 위해서는 self에 해당하는 객체를 넣어주어야 함
 # Charizard.attack(c1)
-print(g1.get_name())
-g1.set_name("잉어킹")
-print(g1.get_name())
+# print(g1.get_name())
+# g1.set_name("잉어킹")
+# print(g1.get_name())
+
+#propertiy
+print(g1.name)
+g1.name = '잉어킹'
+print(g1.hidden_name)
