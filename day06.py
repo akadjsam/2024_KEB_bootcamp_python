a1dd8a4cb95646a13ad5ef91c5b310e4f0eacee2
akadjsam <akadjsam@gmail.com>
2024-01-19 09:16:15 +0900

    v5.3 assignment 9.2 day06

--- day06.py
+++ day06.py
@@ -1,10 +1,15 @@
-def good() ->list:
+def get_odds(n) -> int:
     """
-    chapter 9 things to do. 91.
-    :return: list
+    1 부터 n까지의 홀수를 발생시키는 제너레이터
+    :param n: int
+    :return: int
     """
-
-    harry_porter = input().split()
-    return harry_porter
-
-print(good())
+    for i in range(1,n,2):
+        yield i
+cnt = 0
+odds = get_odds(10)
+for i in odds:
+    cnt += 1
+    if cnt == 3:
+        print(i)
+        break
