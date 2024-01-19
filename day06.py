8324807c37ae062e5ea496e126a5b19cf0b01011
akadjsam <akadjsam@gmail.com>
2024-01-19 13:04:41 +0900

    v5.9 day06

--- day06.py
+++ day06.py
@@ -1,24 +1,7 @@
-import random as ra
-class OopsExaception(Exception):
-    pass
+class Pokemon:
+    def __init__(self, name): #생성자, 객체 생성시 실행되는 함수
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
 
-# numbers = []
-# for i in range(10):
-#     numbers.append(ra.randint(1,100))
-numbers = [ra.randint(1,100) for i in range(10)]
-print(numbers)
-try:
-    pick = int(input(f"Input index (0 ~ {len(numbers)-1}): "))
-    print(numbers[pick])
-    print(5/2)
-    raise OopsExaception("Oops") #코드에 에러가 없어도 에러를 발생시킴("Oops")라는 에러를 출력
-except IndexError as e:
-    print("error : ",e)
-except ValueError as e:
-    print("error : ", e)
-except ZeroDivisionError as e:
-    print("error : ",e)
-except Exception as e: #문법상 except 중 맨 아래에 위치해야 한다.
-    print("error occurs :", e)
-else:
-    print("Program Terminate")
+pikachu = Pokemon("피카츄")
+squirtle = Pokemon("꼬부기")
